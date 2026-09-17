57d9c1091667cc9acf10e397db7679f468775601
laki299 <laki35083@gmail.com>
2026-09-17 16:08:48 +0600

    Add AppConstants class with app configuration

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,26 @@
+class AppConstants {
+  static const String appName = 'গল্পঘর';
+  static const String appNameEnglish = 'GolpoGhor';
+
+  // Content Limits
+  static const int maxImagesPerContent = 1;
+  static const int storyPreviewLength = 180;
+
+  // Categories
+  static const List<String> categories = [
+    'রোমান্টিক',
+    'ইসলামিক',
+    'হরর',
+    'মিস্ট্রি',
+    'থ্রিলার',
+    'অ্যাডভেঞ্চার',
+    'সামাজিক',
+    'পারিবারিক',
+    'ঐতিহাসিক',
+    'সাইন্স ফিকশন',
+    'কবিতা',
+    'ছোটগল্প',
+    'উপন্যাস',
+    'অন্যান্য',
+  ];
+}
