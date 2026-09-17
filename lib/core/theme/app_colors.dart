a888c885caa97f1d67be9e9e34115704f77e359c
laki299 <laki35083@gmail.com>
2026-09-17 16:10:35 +0600

    Add AppColors class for theme color definitions

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Brand
+  static const Color primary = Color(0xFF1A73E8);
+  static const Color primaryDark = Color(0xFF1557B0);
+
+  // Light Theme
+  static const Color lightBackground = Color(0xFFF8F9FA);
+  static const Color lightSurface = Colors.white;
+  static const Color lightCard = Colors.white;
+  static const Color lightTextPrimary = Color(0xFF202124);
+  static const Color lightTextSecondary = Color(0xFF5F6368);
+  static const Color lightBorder = Color(0xFFE8EAED);
+  static const Color lightDivider = Color(0xFFF1F3F4);
+
+  // Dark Theme
+  static const Color darkBackground = Color(0xFF0F0F0F);
+  static const Color darkSurface = Color(0xFF1A1A1A);
+  static const Color darkCard = Color(0xFF1E1E1E);
+  static const Color darkTextPrimary = Color(0xFFE8EAED);
+  static const Color darkTextSecondary = Color(0xFF9AA0A6);
+  static const Color darkBorder = Color(0xFF3C4043);
+  static const Color darkDivider = Color(0xFF2D2D2D);
+
+  // Common
+  static const Color error = Color(0xFFD93025);
+  static const Color success = Color(0xFF188038);
+  static const Color warning = Color(0xFFF9AB00);
+}
