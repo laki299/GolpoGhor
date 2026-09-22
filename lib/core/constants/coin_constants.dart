09f99ea9a4609ef913a112917344f743114b6aa3
laki299 <laki35083@gmail.com>
2026-09-22 23:00:41 +0600

    Add CoinConstants class with predefined values

---
+++ lib/core/constants/coin_constants.dart
@@ -0,0 +1,9 @@
+class CoinConstants {
+  static const int adReward = 100;
+  static const int storyOpen = 50;
+  static const int novelFirstEpisode = 10;
+  static const int novelNextEpisode = 100;
+  static const int reaction = 2;
+  static const int comment = 2;
+  static const int adCooldownSeconds = 60;
+}
