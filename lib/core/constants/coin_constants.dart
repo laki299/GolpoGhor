6273801978d965de457f384f71de7f5a30abccde
laki299 <laki35083@gmail.com>
2026-09-23 19:33:40 +0600

    Add media rates constants for audio and video
    Added constants for audio and video media rates.

--- lib/core/constants/coin_constants.dart
+++ lib/core/constants/coin_constants.dart
@@ -6,4 +6,8 @@ class CoinConstants {
   static const int reaction = 2;
   static const int comment = 2;
   static const int adCooldownSeconds = 60;
+
+  /// Media (default; server may override via app_settings media_rates)
+  static const int audioPer5Seconds = 3;
+  static const int videoPer5Seconds = 30;
 }
